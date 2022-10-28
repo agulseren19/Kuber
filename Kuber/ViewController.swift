5ab8cc17522b401552ae1e972a2a2377caa279a4
agulseren19 <agulseren19@ku.edu.tr>
2022-10-28 16:25:39 +0300

    implement checkbox ui

--- Kuber/ViewController.swift
+++ Kuber/ViewController.swift
@@ -6,19 +6,55 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var fullNameInputField: UITextField!
+    @IBOutlet weak var phoneNumberInputField: UITextField!
+    @IBOutlet weak var majorInputField: UITextField!
+    
+    @IBOutlet weak var checkboxSmoking: UIButton!
+    
+    @IBOutlet weak var checkboxChattiness: UIButton!
+    
+    @IBOutlet weak var gradeSegmentedControl: UISegmentedControl!
+    var smokingFlag=false
+    var chattinessFlag=false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //set font of segmented control
+        let font=UIFont.systemFont(ofSize: 8)
+        UISegmentedControl.appearance().setTitleTextAttributes([NSAttributedString.Key.font: font], for: .normal)
+
     }
 
 
     @IBAction func signUpButtonClicked(_ sender: UIButton) {
       let fullName = fullNameInputField.text
+        let phoneNumber = phoneNumberInputField.text
+        let major = majorInputField.text
     }
     
+    @IBAction func smokingCheckBoxClicked(_ sender: UIButton) {
+        if(smokingFlag==true){
+            sender.setImage((UIImage(named:"uncheckedCheckbox")), for: .normal)
+            smokingFlag=false
+        }
+        else{
+            sender.setImage((UIImage(named:"checkedCheckbox")), for: .normal)
+            smokingFlag=true
+        }
+    }
+    
+    @IBAction func chattinessCheckBoxClicked(_ sender: UIButton) {
+        if(chattinessFlag==true){
+            sender.setImage((UIImage(named:"uncheckedCheckbox")), for: .normal)
+            chattinessFlag=false
+        }
+        else{
+            sender.setImage((UIImage(named:"checkedCheckbox")), for: .normal)
+            chattinessFlag=true
+        }
+    }
 }
 
