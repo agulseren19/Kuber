096c9085705b0ef7ff4c1cff47f784395a3e8951
agulseren19 <agulseren19@ku.edu.tr>
2022-10-29 00:45:25 +0300

    implement login storyboard and view controller

---
+++ Kuber/LogIn/LogInViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LogInViewController.swift
+//  Kuber
+//
+//  Created by Aslıhan Gülseren on 28.10.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+    @IBAction func logInButtonClicked(_ sender: UIButton) {
+        let signUpViewController:UIViewController = self.storyboard?.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
+        //log in using firebase here
+        //if succesful login and first time user
+        print(self.navigationController)
+        self.navigationController?.pushViewController(signUpViewController, animated: true)
+    }
+    
+}
