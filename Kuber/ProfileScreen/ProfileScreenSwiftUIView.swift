a0486c0d7ba48d9a0c09b7fc1edc02b8a5c44eb8
Arda Aliz <ardaaliz01@gmail.com>
2022-10-29 11:58:11 +0300

    Create Profile Screen Branch

---
+++ Kuber/ProfileScreen/ProfileScreenSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileScreenSwiftUIView.swift
+//  Kuber
+//
+//  Created by Arda Aliz on 26.10.2022.
+//
+
+import SwiftUI
+
+struct ProfileScreenSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileScreenSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileScreenSwiftUIView()
+    }
+}
