c11e634a75fda9a283a64c1bd51b8e1d5c0f347d
Arda Aliz <aaliz19@ku.edu.tr>
2022-10-31 15:50:37 +0300

    rearrange the packages

---
+++ Kuber/Controller/ProfileScreenViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileScreenViewController.swift
+//  Kuber
+//
+//  Created by Arda Aliz on 31.10.2022.
+//
+
+import UIKit
+
+class ProfileScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var majorLabel: UILabel!
+    @IBOutlet weak var gradeLevelLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var mailLabel: UILabel!
+    
+    @IBAction func hitchhikeStatusButton(_ sender: Any) {
+    }
+    
+    @IBAction func rideRequestsButton(_ sender: Any) {
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+    }
+    
+    @IBAction func editButton(_ sender: Any) {
+    }
+    
+    @IBAction func homeButton(_ sender: Any) {
+    }
+    
+    @IBAction func profileButton(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
