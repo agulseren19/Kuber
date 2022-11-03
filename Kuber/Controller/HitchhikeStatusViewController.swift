874d9d9f8d1e6e210f0b604a235cc54e30829e25
agulseren19 <agulseren19@ku.edu.tr>
2022-11-03 18:28:15 +0300

    initialized screens and navigations

---
+++ Kuber/Controller/HitchhikeStatusViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HitchhikeStatusViewController.swift
+//  Kuber
+//
+//  Created by Aslıhan Gülseren on 3.11.2022.
+//
+
+import UIKit
+
+class HitchhikeStatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Kuber/Controller/RidesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RidesViewController.swift
+//  Kuber
+//
+//  Created by Aslıhan Gülseren on 3.11.2022.
+//
+
+import UIKit
+
+class RidesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
