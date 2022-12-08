3d2c35e147530e57c70bac9fce246bc507015f84
Utku <ualtintas19@ku.edu.tr>
2022-12-08 11:28:07 +0300

    show all on vs off implemented

--- Kuber/Model/RidesDataSource.swift
+++ Kuber/Model/RidesDataSource.swift
@@ -21,7 +21,50 @@ class RidesDataSource{
     }
     
     func getListOfRidesWithShowAll() {
+        self.ridesArray.removeAll()
         var mutex = 0
+        let db = Firestore.firestore()
+
+        db.collection("rides").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents for Rides Screen: \(err)")
+            } else {
+                self.rideCount = querySnapshot!.count
+                print("querySnapshot!.count:")
+                print(querySnapshot!.count)
+                
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                    
+                    let newRide = Ride (
+                        rideId: document.documentID,
+                        fromLocation: document.get("from") as! String,
+                        toLocation: document.get("to") as! String,
+                        date: (document.get("date") as! Timestamp).dateValue(),
+                        seatAvailable: document.get("numberOfSeats") as! Int,
+                        fee: document.get("fee") as! Int,
+                        mail: document.get("mail") as! String
+                    )
+                    
+                    self.ridesArray.append(newRide)
+                    print("X")
+                    
+                    mutex = mutex + 1
+                    if (mutex == self.rideCount){
+                        DispatchQueue.main.async {
+                            self.delegate?.ridesListLoaded()
+                            print("Y")
+                        }
+                    }
+                }
+                print("Z")
+            }
+        }
+    }
+    
+    func getListOfRidesWithoutShowAll() {
+        var mutex = 0
+        self.ridesArray.removeAll()
         let db = Firestore.firestore()
 
         db.collection("rides").getDocuments() { (querySnapshot, err) in
