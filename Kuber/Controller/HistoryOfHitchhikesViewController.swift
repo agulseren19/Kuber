77357477471f678498695232b879583f13bd1721
Arda Aliz <aaliz19@ku.edu.tr>
2022-12-14 20:30:59 +0300

    Start implementing the two history screens into storyboard

---
+++ Kuber/Controller/HistoryOfHitchhikesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HistoryOfHitchhikesViewController.swift
+//  Kuber
+//
+//  Created by Arda Aliz on 14.12.2022.
+//
+
+import UIKit
+
+class HistoryOfHitchhikesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
